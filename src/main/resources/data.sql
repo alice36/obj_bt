c4ccf1d9d6f8d8113f794578c6cae7bfdd57453b
alice36 <alice.stelmach@gmail.com>
2018-09-27 15:41:11 +0200

    Projekt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,3 @@
-INSERT INTO community(id, name, address) VALUES (1, 'Zielona Brzoza', 'Warszawa');
-INSERT INTO community(id, name, address) VALUES (2, 'Zimowy zakątek', 'Wrocław');
-
-INSERT INTO flat(id, number, area, community_id) VALUES (1, 22, 55.5, 1);
-INSERT INTO flat(id, number, area, community_id) VALUES (2, 33, 85.0, 2);
-INSERT INTO flat(id, number, area, community_id) VALUES (3, 11, 120.0, 1);
-INSERT INTO flat(id, number, area, community_id) VALUES (4, 7, 65.8, 2);
-
-INSERT INTO resident(id, first_name, last_name, gender, flat_id) VALUES (1, 'Zofia', 'Abacka', 'K', 2);
-INSERT INTO resident(id, first_name, last_name, gender, flat_id) VALUES (2, 'Jan', 'Wolski', 'M', 1);
-INSERT INTO resident(id, first_name, last_name, gender, flat_id) VALUES (3, 'Marta', 'Polska', 'K', 2);
-INSERT INTO resident(id, first_name, last_name, gender, flat_id) VALUES (4, 'Krystian', 'Winny', 'M', 3);
-INSERT INTO resident(id, first_name, last_name, gender, flat_id) VALUES (5, 'Pawel', 'Robak', 'M', 4);
-INSERT INTO resident(id, first_name, last_name, gender, flat_id) VALUES (6, 'Anna', 'Wrok', 'K', 3);
-
 INSERT INTO voucher(id, numer, is_available) VALUES (1, '22344', 0);
 INSERT INTO voucher(id, numer, is_available) VALUES (2, '12344', 1);
 INSERT INTO voucher(id, numer, is_available) VALUES (3, '02344', 1);
